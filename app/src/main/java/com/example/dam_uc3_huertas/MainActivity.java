fd1408a5c7230b32b771b0b709d9d27c8afbd630
dafne huertas <nafechg02@gmail.com>
2024-11-20 17:54:44 -0500

    colores listo

--- app/src/main/java/com/example/dam_uc3_huertas/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_huertas/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dam_uc3_huertas;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btnenviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
